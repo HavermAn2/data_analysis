4fb564c9eafb812029b675c3cd0b9d16572966c2
HavermAn2 <olejnikandrej04@gmail.com>
2025-07-13 12:50:03 +0200

    Variebles name was changed

--- scripts/data_visualisation.py
+++ scripts/data_visualisation.py
@@ -9,13 +9,13 @@ print(gdp.columns)
 countrys_name = row["country"]# get countrys name
 print(countrys_name)
 gdp_cols = [col for col in gdp.columns if '_gdp' in col and col != 'country']# leave only columns with "GDP"
-Albania = row[gdp_cols]
-Albania.index = [col.replace('_gdp', '')for col in gdp_cols] #Remove "GDP" becouse we must have only numbers without any letters
-Albania = Albania.replace({ ',': '', ' ': ''}, regex=True)# remove all commas and holes
-Albania = pd.to_numeric(Albania, errors='coerce') #turn all objects to numbers( all values were objects but for visualisation we need numbers)
+countries_statistics = row[gdp_cols]
+countries_statistics.index = [col.replace('_gdp', '')for col in gdp_cols] #Remove "GDP" becouse we must have only numbers without any letters
+countries_statistics = countries_statistics.replace({ ',': '', ' ': ''}, regex=True)# remove all commas and holes
+countries_statistics = pd.to_numeric(countries_statistics, errors='coerce') #turn all objects to numbers( all values were objects but for visualisation we need numbers)
 
 
-print(Albania)
-Albania.plot(kind ='bar')
+print(countries_statistics)
+countries_statistics.plot(kind ='bar')
 plt.title(countrys_name)
 plt.show()
